0f3be8e6cdfa45542043432838b606621cbde8f3
Anthony Deco <anthonydeco322@gmail.com>
2022-01-06 17:16:45 +0800

    Update Subtitle.js

--- src/components/Subtitle.js
+++ src/components/Subtitle.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import propTypes from 'prop-types';
 
 function Subtitle({ children }) {
   return <h3 className="ud-text-sm ud-font-normal ud-text-gray-600">{children}</h3>;
