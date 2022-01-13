21b780a27684d14ca4a1a991a2cb3ebbb7b7ada0
Anthony Deco <anthonydeco322@gmail.com>
2022-01-13 16:09:00 +0800

    grid image fixes

--- src/components/GridImage.js
+++ src/components/GridImage.js
@@ -7,7 +7,7 @@ function GridImage({ imageUrl, title, onClick }) {
         className="ud-flex ud-flex-col ud-rounded-lg ud-w-44 ud-bg-transparent hover:ud-bg-purple-300 ud-cursor-pointer ud-overflow-hidden"
         onClick={onClick}>
         <img className="ud-aspect-square ud-w-full ud-rounded-lg" src={imageUrl} alt="image cover" />
-        <h4 className="ud-px-1 ud-py-1 ud-text-black ud-font-semibold ud-text-center">
+        <h4 className="ud-px-1 ud-py-1 ud-text-black ud-font-semibold ud-truncate ud-text-left">
           {title}
         </h4>
       </div>
