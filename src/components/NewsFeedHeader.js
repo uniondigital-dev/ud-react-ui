6fe41a5a7908d0d483743a9b0b599c5a9c2cc396
Quel Perlado <quelperlado@gmail.com>
2022-01-14 16:16:36 +0800

    Added share props

--- src/components/NewsFeedHeader.js
+++ src/components/NewsFeedHeader.js
@@ -1,17 +1,25 @@
-import React from 'react';
 import Avatar from './Avatar';
 import PropTypes from 'prop-types';
 
-function NewsFeedHeader({ title, subtitle, src, suffixIcon }) {
+function NewsFeedHeader({ title, subtitle, shared, avatarUrl, suffixIcon }) {
   return (
     <div className="ud-flex ud-rounded-lg">
       <div className="ud-flex ud-w-90 ud-flex-auto">
         <div className="ud-m-4">
-          <Avatar src={src} />
+          <Avatar src={avatarUrl} />
         </div>
         <div className="ud-my-4">
-          <h1 className="ud-text-base ud-font-medium">{title}</h1>
-          <h3 className="ud-text-sm ud-text-slate-500 ud--mt-1">{subtitle}</h3>
+          <p className="ud-text-base ud-font-medium">
+            {shared === undefined ? (
+              title
+            ) : (
+              <div className="ud-flex">
+                {title} <p className="ud-text-slate-500 ud-mx-1"> shared </p>{' '}
+                {shared}
+              </div>
+            )}
+          </p>
+          <p className="ud-text-sm ud-text-slate-500 ud--mt-1 ">{subtitle}</p>
         </div>
       </div>
       <div className="ud-flex ud-flex-auto ud-w-10 ud-justify-end ud-items-center ud-m-4">
@@ -24,8 +32,9 @@ function NewsFeedHeader({ title, subtitle, src, suffixIcon }) {
 NewsFeedHeader.propTypes = {
   title: PropTypes.string.isRequired,
   subtitle: PropTypes.string.isRequired,
-  src: PropTypes.string.isRequired,
+  avatarUrl: PropTypes.string.isRequired,
   suffixIcon: PropTypes.node,
+  shared: PropTypes.string,
 };
 
 export default NewsFeedHeader;
