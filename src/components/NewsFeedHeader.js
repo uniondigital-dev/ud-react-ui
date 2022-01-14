0d891429a55146477d5bf0b69a32013d91db0ba0
Quel Perlado <quelperlado@gmail.com>
2022-01-14 08:45:34 +0800

    Update NewsFeedHeader.js

--- src/components/NewsFeedHeader.js
+++ src/components/NewsFeedHeader.js
@@ -3,24 +3,20 @@ import Avatar from './Avatar';
 import PropTypes from 'prop-types';
 import { FaHeart } from 'react-icons/fa';
 
-function NewsFeedHeader({ title, subtitle, src, options, suffixIcon }) {
-  options = 'heart';
-
-  if (options === 'heart') {
-    options === 'heart';
-  }
-
+function NewsFeedHeader({ title, subtitle, src, suffixIcon }) {
   return (
     <div className="ud-flex ud-shadow-md ud-rounded-lg">
-      <div className="ud-m-2">
-        <Avatar src={src} />
+      <div className="ud-flex ud-w-90 ud-flex-auto">
+        <div className="ud-m-4">
+          <Avatar src={src} />
+        </div>
+        <div className="ud-my-4">
+          <h1 className="ud-text-base ud-font-medium">{title}</h1>
+          <h3 className="ud-text-sm ud-text-slate-500 ud--mt-1">{subtitle}</h3>
+        </div>
       </div>
-      <div className="ud-m-2 ud-flex-auto ud-w-90">
-        <h1 className="ud-text-base ud-font-medium">{title}</h1>
-        <h3 className="ud-text-sm ud-text-slate-500 ud--mt-1">{subtitle}</h3>
-      </div>
-      <div className="ud-flex-auto ud-w-10 ">
-        <div className="ud-justify-center ud-items-center">{suffixIcon}</div>
+      <div className="ud-flex ud-flex-auto ud-w-10 ud-justify-end ud-items-center ud-m-4">
+        <div>{suffixIcon}</div>
       </div>
     </div>
   );
