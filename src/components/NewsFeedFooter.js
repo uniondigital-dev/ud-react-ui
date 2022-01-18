3f712c2db0afbd9052598ea3416164fbe633abfa
Quel Perlado <quelperlado@gmail.com>
2022-01-18 10:37:49 +0800

    Changes
    - Updated Proptypes into prefix and suffix for more control of the footer

--- src/components/NewsFeedFooter.js
+++ src/components/NewsFeedFooter.js
@@ -3,25 +3,22 @@ import AvatarList from './AvatarList';
 import Subtitle from './Subtitle';
 import PropTypes from 'prop-types';
 
-function NewsFeedFooter({ avatarUsers, avatarLabel, suffixIcon }) {
+function NewsFeedFooter({ prefix, suffix }) {
   return (
     <div className="ud-flex">
       <div className="ud-flex ud-w-90">
-        <div className="">
-          <AvatarList users={avatarUsers} label={avatarLabel} />
-        </div>
+        <div className="">{prefix}</div>
       </div>
       <div className="ud-flex ud-flex-auto ud-w-10 ud-justify-end ud-items-center">
-        <div>{suffixIcon}</div>
+        <div>{suffix}</div>
       </div>
     </div>
   );
 }
 
 NewsFeedFooter.PropTypes = {
-  avatarUsers: PropTypes.func.isRequired,
-  avatarLabel: PropTypes.string.isRequired,
-  suffixIcon: PropTypes.node,
+  prefix: PropTypes.node.isRequired,
+  suffix: PropTypes.node,
 };
 
 export default NewsFeedFooter;
