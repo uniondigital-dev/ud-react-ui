42db76078ea60dc504624739ed4e5ad899298e05
Quel Perlado <quelperlado@gmail.com>
2022-01-21 12:10:57 +0800

    Changes Applied
    - Whole Fundraising Card implemented

--- src/components/NewsFeedFundraisingCard.js
+++ src/components/NewsFeedFundraisingCard.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { NewsFeedFooter, NewsFeedHeader } from '..';
 
-function NewsFeedFundraisingCard({ image, content }) {
+function NewsFeedFundraisingCard({
+  title,
+  subtitle,
+  shared,
+  avatarUrl,
+  suffixIcon,
+  image,
+  content,
+  prefix,
+  suffix,
+}) {
   return (
-    <div className="ud-flex ud-flex-col ">
+    <div className="ud-flex ud-flex-col ud-rounded-lg ud-shadow-md">
+      <div className="ud-px-5 ud-py-5">
+        <NewsFeedHeader
+          title={title}
+          subtitle={subtitle}
+          shared={shared ? 'fundraising' : undefined}
+          avatarUrl={avatarUrl}
+          suffixIcon={suffixIcon}
+        />
+      </div>
       <img className="ud-aspect-video ud-object-cover" src={image} />
-      <p className="ud-m-8 ud-font-medium">{content}</p>
+      <p className="ud-px-8 ud-py-7 ud-font-medium">{content}</p>
+      <div className="ud-pb-7 ud-px-5 ">
+        <NewsFeedFooter prefix={prefix} suffix={suffix} />
+      </div>
     </div>
   );
 }
