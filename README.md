9884810298666d6f14663dee8e7aeda4c9920f19
mhackyu <mark.christian.paderes@gmail.com>
2022-01-24 11:16:33 +0800

    FIX: Dedupe react dependency

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Union Digital's Mini App React Components
+
+# Issues
+
+### Duplicate react dependency
+
+Assuming `myapp` and `ud-react-ui` are sibling folders, one possible fix is to run this command:
+
+```shell
+npm link ../<myapp-folder>/node_modules/react
+```
--- src/components/GridImageList.js
+++ src/components/GridImageList.js
@@ -6,34 +6,44 @@ import Button from './Button';
 import Title from './Title';
 import VSpace from './VSpace';
 
-function GridImageList({listTitle, items, listSliceCount, contextButtonTitle = 'See All', onItemClick, onContextButtonClick,}) {
-    return (
-      <div className="ud-flex ud-flex-col ud-space-x-2">
-        <div className="ud-flex ud-ml-2 ud-items-center ud-justify-between">
-          <Title>{listTitle}</Title>
-          <Button
-            variant="link"
-            onClick={onContextButtonClick}
-          >
-            {contextButtonTitle}
-          </Button>
-        </div>
-        <VSpace/>
-        <div className="ud-grid ud-grid-cols-2 ud-gap-4">
-          {items.slice(0,listSliceCount ?? items.length).map((item, index) => (
-            <GridImage key={index} title={item.title} imageUrl={item.imageUrl} onClick={() => onItemClick(item)} hasBorder />
-          ))}
-        </div>
+function GridImageList({
+  listTitle,
+  items,
+  listSliceCount,
+  contextButtonTitle = 'See All',
+  onItemClick,
+  onContextButtonClick,
+}) {
+  return (
+    <div className="ud-flex ud-flex-col ud-space-x-2">
+      <div className="ud-flex ud-ml-2 ud-items-center ud-justify-between">
+        <Title>{listTitle}</Title>
+        <Button variant="link" onClick={onContextButtonClick}>
+          {contextButtonTitle}
+        </Button>
       </div>
-    );
-  }
-  
-  GridImageList.propTypes = {
-    listTitle: PropTypes.string.isRequired,
-    items: PropTypes.array.isRequired,
-    listSliceCount: PropTypes.number,
-    onItemClick: PropTypes.func,
-    onContextButtonClick: PropTypes.func,
-  };
-  
-  export default GridImageList;
+      <VSpace />
+      <div className="ud-grid ud-grid-cols-2 ud-gap-4">
+        {items.slice(0, listSliceCount ?? items.length).map((item, index) => (
+          <GridImage
+            key={index}
+            title={item.title}
+            imageUrl={item.imageUrl}
+            onClick={() => onItemClick(item)}
+            hasBorder
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+GridImageList.propTypes = {
+  listTitle: PropTypes.string.isRequired,
+  items: PropTypes.array.isRequired,
+  listSliceCount: PropTypes.number,
+  onItemClick: PropTypes.func,
+  onContextButtonClick: PropTypes.func,
+};
+
+export default GridImageList;
